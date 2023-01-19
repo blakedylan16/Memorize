1021e6fbc5beedde843e45e57d007eaf095dc056
blakedylan16 <dylanblake@nyu.edu>
2023-01-19 17:37:19 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Dylan Blake on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
