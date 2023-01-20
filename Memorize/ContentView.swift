6aee215287e5d6fb2b85fff0ba87caeaf8ab8efc
blakedylan16 <dylanblake@nyu.edu>
2023-01-20 00:30:11 -0500

    A commit but using a branch this time. Hopefully this works.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("TESTING GIT COMMANDS")
+            Text("TESTING GIT COMMANDS P2")
         }
         .padding()
     }
