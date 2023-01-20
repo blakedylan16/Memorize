00fa1ec24269c3acf4ceecf5bdb73a45662f9057
blakedylan16 <dylanblake@nyu.edu>
2023-01-19 23:24:29 -0500

    First commit! (Part 2; last time I forgot to select "Push to remote")

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Bonjour!")
+            Text("Hello, CS193p!")
         }
         .padding()
     }
