953b99fc2d269948136d5c01c3adc35d0af7e98b
blakedylan16 <dylanblake@nyu.edu>
2023-01-22 00:15:03 -0500

    i don't remember which commit attempt this is

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,35 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20.0)
-                .stroke(lineWidth: 3)
-            
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!")
-                    .foregroundColor(Color.blue)
-            }
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
         }
         .padding(.horizontal)
         .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
-        
-        
-
-        
-        
-        
-        
-        
-        
-    }
 }
 
 
 
-
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20.0)
+                .stroke(lineWidth: 3)
+            Text("✈️")
+        }
+        
+        
+    }
+    }
+}
 
 
 
