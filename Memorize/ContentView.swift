19a28099dcf27f1f83d0becd70b04024d1a0f097
blakedylan16 <dylanblake@nyu.edu>
2023-01-23 15:40:22 -0500

    Finished Lecture 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,47 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var emojis = ["✈️", "🚑", "🚂", "⛴️", "🚗", "🚕", "🚙", "🚌", "🚎", "🏎️", "🚓", "🚒", "🚐", "🛻", "🚚", "🚛", "🚜", "🛵", "🚲", "🏍️", "🛺", "🚀"]
+    
     @State var emojiCount = 6
+    
     var body: some View {
-        VStack{
-            HStack {
-                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
-                    CardView(content: emoji)
+        VStack {
+            ScrollView{
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
                 }
             }
-            HStack{
+            .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+            Spacer(minLength: 20)
+            HStack {
                 remove
                 Spacer()
                 add
             }
+            .font(.largeTitle)
             .padding(.horizontal)
         }
         .padding(.horizontal)
-        .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
         
     }
     
     
     var remove: some View {
-        Button(action: {
-            emojiCount -= 1
-        }, label: {
-            VStack {
-                Text("Remove")
-                Text("Card")
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
             }
-        })
+        } label: {
+            Image(systemName: "minus.circle")
+        }
     }
     
     var add: some View {
-        Button(action: {
-            emojiCount += 1
-        }, label: {
-            VStack {
-                Text("Add")
-                Text("Card")
+        Button {
+            if emojiCount < emojis.count{
+                emojiCount += 1
             }
-        })
+        } label: {
+            Image(systemName: "plus.circle")
+        }
     }
 }
 
@@ -61,19 +66,17 @@ struct CardView: View {
      @State changes isFaceUp to something that points to a boolean rather than being an immutable boolean so when changing the variable, you actually change what its referencing. This is for "changing" immutable data types. We don't use this often in app dev.
      */
     
-    @State var content: String
+    var content: String
     var body: some View {
         ZStack {
-            
             let shape = RoundedRectangle(cornerRadius: 20)
-            
             if isFaceUp {
                 shape
                     .fill()
                     .foregroundColor(.white)
                 shape
-                    .stroke(lineWidth: 3)
-                Text("✈️")
+                    .strokeBorder(lineWidth: 3)
+                Text(content)
                     .font(.largeTitle)
             }
             else {
@@ -81,16 +84,11 @@ struct CardView: View {
                     .fill()
             }
         }
-        onTapGesture {
+        .onTapGesture {
             isFaceUp = !isFaceUp
         }
-        
-        
     }
 }
-
-
-
 
 
 struct ContentView_Previews: PreviewProvider {
