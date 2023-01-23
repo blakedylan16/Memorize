dcec88e4da675a2e8721075fb54b1b2aac92f74c
blakedylan16 <dylanblake@nyu.edu>
2023-01-22 21:44:07 -0500

    In the middle of the second lecture

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,29 +8,84 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["✈️", "🚑", "🚂", "⛴️", "🚗", "🚕", "🚙", "🚌", "🚎", "🏎️", "🚓", "🚒", "🚐", "🛻", "🚚", "🚛", "🚜", "🛵", "🚲", "🏍️", "🛺", "🚀"]
+    @State var emojiCount = 6
     var body: some View {
-        HStack {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+        VStack{
+            HStack {
+                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    CardView(content: emoji)
+                }
+            }
+            HStack{
+                remove
+                Spacer()
+                add
+            }
+            .padding(.horizontal)
         }
         .padding(.horizontal)
         .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+        
+    }
+    
+    
+    var remove: some View {
+        Button(action: {
+            emojiCount -= 1
+        }, label: {
+            VStack {
+                Text("Remove")
+                Text("Card")
+            }
+        })
+    }
+    
+    var add: some View {
+        Button(action: {
+            emojiCount += 1
+        }, label: {
+            VStack {
+                Text("Add")
+                Text("Card")
+            }
+        })
+    }
 }
 
 
-
 struct CardView: View {
+    @State var isFaceUp: Bool = true
+    
+    /*
+     @State changes isFaceUp to something that points to a boolean rather than being an immutable boolean so when changing the variable, you actually change what its referencing. This is for "changing" immutable data types. We don't use this often in app dev.
+     */
+    
+    @State var content: String
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 20.0)
-                .stroke(lineWidth: 3)
-            Text("✈️")
+            
+            let shape = RoundedRectangle(cornerRadius: 20)
+            
+            if isFaceUp {
+                shape
+                    .fill()
+                    .foregroundColor(.white)
+                shape
+                    .stroke(lineWidth: 3)
+                Text("✈️")
+                    .font(.largeTitle)
+            }
+            else {
+                shape
+                    .fill()
+            }
+        }
+        onTapGesture {
+            isFaceUp = !isFaceUp
         }
         
         
-    }
     }
 }
 
@@ -41,5 +96,8 @@ struct CardView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
